e4204ee90ff406b53a18960a08e582439a87e140
EndarGayuh <endargayuhmuktitama@gmail.com>
2018-11-17 16:15:32 +0700

    Initial commit

---
+++ app/src/main/java/com/muktitama/modulretrofit/MainActivity.java
@@ -0,0 +1,58 @@
+package com.muktitama.modulretrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.muktitama.modulretrofit.Adapters.MoviesAdapter;
+import com.muktitama.modulretrofit.Models.Movie;
+import com.muktitama.modulretrofit.Models.MovieResponse;
+import com.muktitama.modulretrofit.Rest.ApiClient;
+import com.muktitama.modulretrofit.Rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final static String API_KEY = "e371e8b60f5c78d50b63987a13597ccc";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Please Obtain Your API KEY First From themoviedb.org",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                int statusCode = response.code();
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies,
+                        R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+
+        }); }
+}
